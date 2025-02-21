b1d80a75b89c5c83630468bba0e95a2f6117a1ed
Rachel Gottlieb <rachel@MacBook-Air.local>
2025-02-21 16:19:16 -0500

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/updated-exercise-app/', 
+  base: '/updated-exercise-app1/', 
   plugins: [react()],
 });
